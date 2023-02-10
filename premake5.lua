be612d7b15a5093547aaa7524294406f150334d6
Chandrapal Singh <contact@chandrapal.dev>
2023-02-10 18:27:33 +0530

    Added Window class and GLFW.

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,6 @@
 workspace "Hazel"
 	architecture "x64"
+	startproject "Sandbox"
 
 	configurations
 	{
@@ -10,7 +11,6 @@ workspace "Hazel"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-startproject "Sandbox"
 
 -- Include directories relative to root folder (solution directory)
 IncludeDir = {}
