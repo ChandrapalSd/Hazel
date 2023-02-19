c432043b97fb8a06403784817a6ef9a42c841fea
Chandrapal Singh <contact@chandrapal.dev>
2023-02-19 16:31:14 +0530

    Added RendererAPI and RenderCommand architecture.

---
+++ Hazel/src/Hazel/Renderer/RenderCommand.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "RendererAPI.h"
+
+namespace Hazel {
+
+	class RenderCommand
+	{
+	public:
+		void temp();
+		static void SetClearColor(const glm::vec4& color) 
+		{ 
+			s_RendererAPI->SetClearColor(color); 
+		}
+
+		static void Clear() 
+		{ 
+			s_RendererAPI->Clear(); 
+		}
+
+		static void DrawIndexed(const std::shared_ptr<VertexArray>& vertexArray) 
+		{ 
+			return s_RendererAPI->DrawIndexed(vertexArray); 
+		}
+
+	private:
+		static RendererAPI* s_RendererAPI;
+	};
+
+}
